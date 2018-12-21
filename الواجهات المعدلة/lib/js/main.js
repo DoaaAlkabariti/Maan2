064fc8668248b3b4fdad1e470827daf7ccf60fd9
doaa <dodo_wk_94@hotmail.com>
2018-12-21 21:01:50 +0300

    BootStrap Template
    BootStrap Template

---
+++ الواجهات المعدلة/lib/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+        // Transition effect for navbar
+        $(window).scroll(function() {
+          // checks if window is scrolled more than 500px, adds/removes solid class
+          if($(this).scrollTop() > 300) {
+              $('.navbar').addClass('solid');
+          } else {
+              $('.navbar').removeClass('solid');
+          }
+        });
+});
+
+// ***********************************************************************
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+/*************************************************************************
+    $("#show").click(function(){
+        $("aaa").show();
+    });
